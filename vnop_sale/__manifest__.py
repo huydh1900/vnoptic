f0018cf25995a42b91472965d237d2757394b729
huytq <huytq@thinhcuong.vn>
2026-01-23 14:58:54 +0700

    Fix vnoptic

--- vnop_sale/__manifest__.py
+++ vnop_sale/__manifest__.py
@@ -2,14 +2,10 @@
 {
     'name': "vnop_sale",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'sale', 'xnk_intergration', 'stock'],
+    'depends': ['base', 'product', 'sale'],
 
     # always loaded
     'data': [
         'security/groups.xml',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/product_views.xml',
     ],
 }
-
--- vnoptic_inventory/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Load file chứa logic thống kê chính
-from . import inventory_statistic
