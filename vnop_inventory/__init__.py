216f98c8d29a2d6a1977e9f8936b73632737c770
Nguyễn Bảo An <khiconst4@gmail.com>
2026-01-28 16:24:29 +0700

    commit xóa import controller trong init

--- vnop_inventory/__init__.py
+++ vnop_inventory/__init__.py
@@ -2,8 +2,3 @@
 # Import folder models để load các class logic
 from . import models
 
-# Import folder controllers (nếu sau này cần API hoặc route riêng)
-from . import controllers
-
-# Import folder utils (chứa các hàm tiện ích chung)
-from . import utils
