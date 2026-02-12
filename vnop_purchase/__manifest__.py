3676d87bcc5a8ee934bf572d03a7b8abb3b57ecf
Trần Quang Huy <90753684+huydh1900@users.noreply.github.com>
2026-02-12 10:44:04 +0700

    feat(purchase): implement import contract flow and remaining qty wizard

--- vnop_purchase/__manifest__.py
+++ vnop_purchase/__manifest__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': "vnop_purchase",
-    # always loaded
     'depends': ['purchase', 'vnop_delivery'],
     'data': [
         'security/ir.model.access.csv',
+        'data/ir_sequence_data.xml',
         'views/purchase_order_views.xml',
+        'views/import_contract_views.xml',
+        'views/import_contract_wizard_views.xml',
     ],
 }
