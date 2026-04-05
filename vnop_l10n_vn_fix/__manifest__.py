2d9803d56e762ba0789a21477b261b77dfbd5630
huytq <huytq@thinhcuong.vn>
2026-04-05 20:43:06 +0700

    Update vnop_l10n_vn_fix

---
+++ vnop_l10n_vn_fix/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vnop_l10n_vn_fix",
+    'summary': "Fix l10n_vn WIP account template reference (chart154 -> chart1541)",
+    'depends': ['l10n_vn'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
